ed1f46a78e1bb681420d2cb69bd7d45701f54f05
mahm0ud-hamed <mahmoudmohamedhmd@gmail.com>
2024-09-06 14:26:40 +0300

    Media playe Replay Scene

---
+++ GUI/src/mediaplayer/LiveViewController.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mediaplayer;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import java.util.logging.Logger;
+
+/**
+ * FXML Controller class
+ *
+ * @author asem
+ */
+public class LiveViewController implements Initializable {
+
+    @FXML
+    private Button showReplay;
+    @FXML
+    private BorderPane borderPane;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void ShowRepl/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mediaplayer;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import java.util.logging.Logger;
+
+/**
+ * FXML Controller class
+ *
+ * @author asem
+ */
+public class LiveViewController implements Initializable {
+
+    @FXML
+    private Button showReplay;
+    @FXML
+    private BorderPane borderPane;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void ShowReplayAction(ActionEvent event) {
+            try { 
+            showReplay.setVisible(false);
+            BorderPane Live = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+            borderPane.setCenter(Live);
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
